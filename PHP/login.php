29d411015f08aa4d0b4cc92abbfdf3ee15704c2f
tùng <tungvu27005@gmail.com>
2024-12-01 17:27:51 +0700

    update

---
+++ PHP/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>QLSV</title>
+    <style>
+    </style>
+</head>
+
+<body>
+    <div id="loginAccount">
+        <h2>Đăng nhập</h2>
+        <div class="login_box">
+            <form action="" method="post" class="login">
+                <input type="text" placeholder="Tài khoản" name="admin" required>
+                🔒:<input type="password" placeholder="Mật khẩu" name="password"  required>
+                <button id="but" type="submit">Đăng nhập</button>
+                <button type="button">Đăng ký</button>
+            </form>
+        </div>
+        <div class="login_link">
+            <br>
+            <a href="">Quên mật khẩu</a><br />
+            <a href="">Quay lại trang chủ</a>
+        </div>
+    </div>
+</body>
+
+</html>
